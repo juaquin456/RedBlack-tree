469811c6cc815507b4799b039b66e90b88a7d5c0
juaquin456 <juaquin.remon@utec.edu.pe>
2023-11-05 00:06:25 -0500

    Insertion complete

--- RB.h
+++ RB.h
@@ -25,6 +25,16 @@ void init(struct RB_tree* tree) {
     tree->root = NULL;
 }
 
+struct RB_node* new_node(int value) {
+    struct RB_node* node = (struct RB_node*) malloc(sizeof(struct RB_node));
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = NULL;
+    node->color = true;
+    node->value = value;
+    return node;
+}
+
 void _print(struct RB_node* node, int level) {
     if (node->right) {
         _print(node->right, level + 1);
@@ -56,94 +66,135 @@ bool is_black(struct RB_node* node) {
     }
 }
 
-void left_rotate(struct RB_node* node) {
+void left_rotate(struct RB_tree* tree, struct RB_node* node) {
     printf("left rotate\n");
-    struct RB_node* parent = node->parent;
-    struct RB_node* left = node->left;
-    node->parent = parent->parent;
-    node->left = parent;
-    parent->right = left;
-}
-
-void right_rotate(struct RB_node* node) {
-    printf("right rotate\n");
-    struct RB_node* parent = node->parent;
     struct RB_node* right = node->right;
-    node->parent = parent->parent;
-    node->right = parent;
-    parent->left = right;
+    node->right = right->left;
+    if (right->left)
+        right->left->parent = node;
+    if (!node->parent)
+        tree->root = right;
+    else if (node->parent->left == node)
+        node->parent->left = right;
+    else
+        node->parent->right = right;
+    right->parent = node->parent;
+    right->left = node;
+    node->parent = right;
 }
 
-void flip(struct RB_node* node) {
-    node->color = !node->color;
-    if (node->left)
-        node->left->color = !node->left->color;
-    if (node->right)
-        node->right->color = !node->right->color;
+void right_rotate(struct RB_tree* tree, struct RB_node* node) {
+    printf("right rotate\n");
+    struct RB_node* left = node->left;
+    node->left = left->right;
+    if (left->right)
+        left->right->parent = node;
+    if (!node->parent)
+        tree->root = left;
+    else if (node->parent->left == node)
+        node->parent->left = left;
+    else
+        node->parent->right = left;
+    left->parent = node->parent;
+    left->right = node;
+    node->parent = left;
 }
 
-struct RB_node* _insert(struct RB_node* node, int value) {
-    if (node->value > value) {
-        if (node->left) {
-            node->left = _insert(node->left, value);
-        }
-        else {
-            node->left = (struct RB_node *) malloc(sizeof(struct RB_node));
-            node->left->left = NULL;
-            node->left->right = NULL;
-            node->left->color = true;
-            node->left->value = value;
-            node->left->parent = node;
-            if (!is_black(node) & !is_black(node->left)) {
-                if (is_black(node->parent->right)) {
-                    right_rotate(node);
-                    return node;
+void insert_fix(struct RB_tree* tree, struct RB_node* new_node) {
+    struct RB_node* tmp;
+    while (!is_black(new_node->parent)) {
+        if (new_node->parent == new_node->parent->parent->left) {
+            tmp = new_node->parent->parent->right;
+
+            if (!is_black(tmp)) { // CASE 1
+                tmp->color = 0;
+                new_node->parent->color = 0;
+                new_node->parent->parent->color = 1;
+                new_node = new_node->parent->parent;
+            } else {
+                // CASE 2
+                if (new_node == new_node->parent->right) {
+                    new_node = new_node->parent;
+                    left_rotate(tree, new_node);
                 }
-                else {
-                    flip(node);
+
+                // CASE 3
+                new_node->parent->color = 0;
+                new_node->parent->parent->color = 1;
+                right_rotate(tree, new_node->parent->parent);
+            }
+        } else {
+            tmp = new_node->parent->parent->left;
+            if (!is_black(tmp)) {
+                tmp->color = 0;
+                new_node->parent->color = 0;
+                new_node->parent->parent->color = 1;
+                new_node = new_node->parent->parent;
+            } else {
+                if (new_node == new_node->parent->left) {
+                    new_node = new_node->parent;
+                    right_rotate(tree, new_node);
                 }
+                new_node->parent->color = 0;
+                new_node->parent->parent->color = 1;
+                left_rotate(tree, new_node->parent->parent);
             }
         }
     }
-    else {
-        if (node->right) {
-            node->right = _insert(node->right, value);
+    tree->root->color = 0;
+}
+
+struct RB_node* find_parent(struct RB_node* initial, struct RB_node* finding) {
+    struct RB_node* parent = NULL;
+    while (initial) {
+        parent = initial;
+        if (finding->value < initial->value) {
+            initial = initial->left;
         }
         else {
-            node->right = (struct RB_node *) malloc(sizeof(struct RB_node));
-            node->right->left = NULL;
-            node->right->right = NULL;
-            node->right->color = true;
-            node->right->value = value;
-            node->right->parent = node;
-            if (!is_black(node) & !is_black(node->right)) {
-                if (is_black(node->parent->left)) {
-                    left_rotate(node);
-                    return node;
-                }
-                else {
-                    flip(node);
-                }
-            }
+            initial = initial->right;
         }
     }
-    return node;
+    return parent;
 }
 
 void insert(struct RB_tree* tree, int value) {
+    struct RB_node* nn = new_node(value);
+
     if (tree->root == NULL) {
-        tree->root = (struct RB_node*) malloc(sizeof(struct RB_node));
-        tree->root->left = NULL;
-        tree->root->right = NULL;
-        tree->root->color = false;
-        tree->root->value = value;
-        tree->root->parent = NULL;
+        tree->root = nn;
+        nn->color = false;
     }
     else {
-        tree->root = _insert(tree->root, value);
+        struct RB_node* parent = find_parent(tree->root, nn);
+
+        nn->parent = parent;
+        if (nn->value < parent->value)
+            parent->left = nn;
+        else
+            parent->right = nn;
+
+        if (nn->parent->parent)
+            insert_fix(tree, nn);
     }
 }
 
+struct RB_node* pop_min(struct RB_tree* tree) {
+    struct RB_node* tmp = tree->root;
+    while (tmp->left) {
+        tmp = tmp->left;
+    }
+    if (tmp->parent->left == tmp) {
+        tmp->parent->left = tmp->right;
+    }
+    else {
+        tmp->parent->right = tmp->right;
+    }
+    if (tmp->right) {
+        tmp->right->parent = tmp->parent;
+    }
+    return tmp;
+}
 
 
 
--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 int main() {
     struct RB_tree tree;
     init(&tree);
+    // test
     insert(&tree, 10);
     insert(&tree, 5);
     insert(&tree, 1);
-//    insert(&tree, 3);
+    insert(&tree, 11);
+    insert(&tree, 12);
+    insert(&tree, 13);
+    insert(&tree, 14);
+    insert(&tree, 7);
+    insert(&tree, 15);
     print(&tree);
 
     return 0;
